3f9d8d2e9b18d3b93e2acf4f830bfff58f64264e
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-05-14 01:48:05 +0600

    Create project and add some components

---
+++ src/Pages/Home/Banner/CursorPosition/CursorPosition.jsx
@@ -0,0 +1,14 @@
+const CursorPosition = () => {
+  return (
+    <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 left-5 right-5 bottom-0">
+      <a href="#slide4" className="btn btn-circle">
+        ❮
+      </a>
+      <a href="#slide6" className="btn btn-circle">
+        ❯
+      </a>
+    </div>
+  );
+};
+
+export default CursorPosition;
---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from "../Banner/Banner";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <h2>This is Home</h2>
+    </div>
+  );
+};
+
+export default Home;
